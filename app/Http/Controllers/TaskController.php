b724c14e07915f89db6166f5ff42858e75ab321a
ammaar <muhammadfarroos12@gmail.com>
2023-02-01 11:20:19 +0700

    clean up bad code error validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Task;
@@ -32,13 +33,7 @@ class TaskController extends Controller
         return view('task.create');
     }
 
-    public function store(Request $request){
-
-        $request->validate([
-            'task' => ['required'],
-            'user' => ['required']
-        ]);
-
+    public function store(TaskRequest $request){
         Task::create([
             'task' => $request->task,
             'user' => $request->user
@@ -52,7 +47,7 @@ class TaskController extends Controller
         return view('task.edit', compact('task'));
     }
     
-    public function update(Request $request, $id){
+    public function update(TaskRequest $request, $id){
         $task = Task::find($id);
         $task->update([
             'task' => $request->task,
